00d6d148899f84e22eff9c26ec85719a538d5b83
Esmaeil MIRZAEE <esmaeilmirzaee@gmail.com>
2020-09-12 01:10:33 -0400

    Add Render function

---
+++ 036/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"webvideos/036/views"
+
+	"github.com/gorilla/mux"
+)
+
+var (
+	homeView *views.View
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	must(homeView.Render(w, nil))
+}
+
+func main() {
+	homeView = views.NewView("bootstrap", "views/home.gohtml")
+	r := mux.NewRouter()
+	r.HandleFunc("/", home)
+	fmt.Println("Listen & Serve")
+	http.ListenAndServe(":3000", r)
+}
+
+func must(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
