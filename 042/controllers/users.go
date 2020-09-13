fccc87c4947cceffed3b97b9ac536a332b5dac51
Esmaeil MIRZAEE <esmaeilmirzaee@gmail.com>
2020-09-13 10:34:59 -0400

    Get post form data

--- 042/controllers/users.go
+++ 042/controllers/users.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"net/http"
 	"webvideos/042/views"
+
+	"github.com/gorilla/schema"
 )
 
 func NewUsers() *Users {
@@ -17,9 +19,26 @@ type Users struct {
 }
 
 func (u *Users) New(w http.ResponseWriter, r *http.Request) {
-	u.NewView.Render(w, nil)
+	if err := u.NewView.Render(w, nil); err != nil {
+		panic(err)
+	}
+}
+
+type SignupForm struct {
+	Email    string `schema:"email"`
+	Password string `schema:"password"`
 }
 
 func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "User created")
+	fmt.Fprintln(w, "User created")
+	if err := r.ParseForm(); err != nil {
+		panic(err)
+	}
+
+	dec := schema.NewDecoder()
+	var form SignupForm
+	if err := dec.Decode(&form, r.PostForm); err != nil {
+		panic(err)
+	}
+	fmt.Fprintln(w, form.Email)
 }
