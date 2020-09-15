e68cd3e97a984b126f2fde9a8cdc3c0b42eb906d
Esmaeil MIRZAEE <esmaeilmirzaee@gmail.com>
2020-09-15 03:40:43 -0400

    Create user helper

--- 060/models/users.go
+++ 060/models/users.go
@@ -1,12 +1,63 @@
 package models
 
 import (
-	_ "gorm.io/drivers/postgres"
+	"errors"
+
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
-type Users struct {
+var (
+	ErrNotFound = errors.New("models: resources not found.")
+)
+
+type User struct {
 	gorm.Model
 	Name  string
 	Email string `gorm:"not null;unique_index"`
+}
+
+type UserService struct {
+	db *gorm.DB
+}
+
+// NewUserService returns a database connection
+//
+//
+func NewUserService(connectionInfo string) (*UserService, error) {
+	db, err := gorm.Open(postgres.Open(connectionInfo), &gorm.Config{})
+	if err != nil {
+		return nil, err
+	}
+	return &UserService{
+		db: db,
+	}, nil
+}
+
+// ByID returns user
+func (us *UserService) ByID(id uint) (*User, error) {
+	var user User
+	err := us.db.Where("id = ?", id).First(&user).Error
+	switch err {
+	case nil:
+		return &user, nil
+	case gorm.ErrRecordNotFound:
+		return nil, ErrNotFound
+	default:
+		return nil, err
+	}
+}
+
+// Close method closes user service database connection.
+func (us *UserService) Close() error {
+	return nil
+	// us.db.Close()
+}
+
+func (us *UserService) DestructiveReset(hardRest bool) error {
+	if hardRest {
+		return us.db.Migrator().DropTable(&User{})
+	} else {
+		return us.db.AutoMigrate(&User{})
+	}
 }
---
+++ 060/xp/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"webvideos/060/models"
+
+	"gorm.io/gorm"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	username = "postgres"
+	password = "password"
+	dbname   = "tb"
+)
+
+type User struct {
+	gorm.Model
+	Email string `gorm:"not null;unique"`
+	Name  string
+}
+
+func main() {
+	sqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, username, password, dbname)
+	us, err := models.NewUserService(sqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer us.Close()
+
+	user, err := us.ByID(2)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(user)
+	// if err := us.DestructiveReset(false); err != nil {
+	// panic(err)
+	// }
+}
